d351cd80151d043366a3906c2e115f37a2076469
Noamshalev1 <113979512+Noamshalev1@users.noreply.github.com>
2023-04-07 20:54:12 +0300

    DFS - Changes

--- ATP-Project-PartA/src/algorithms/mazeGenerators/search/DepthFirstSearch.java
+++ ATP-Project-PartA/src/algorithms/mazeGenerators/search/DepthFirstSearch.java
@@ -1,62 +1,59 @@
 package algorithms.mazeGenerators.search;
 
+import javafx.util.Pair;
+
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
 
 /* DFS */
-public class DepthFirstSearch extends ASearchingAlgorithm{
+public class DepthFirstSearch extends ASearchingAlgorithm {
 
     private int time;
 
-    public DepthFirstSearch(){
+    public DepthFirstSearch() {
 
     }
+
     /**
      * DFS algorithm
      * Two functions used to set the distance and parents of states
      * @param searchProblem to solve
+     * @param start pose
      */
-    private void DFS(ISearchable searchProblem)
-    {
+    public void DFS(ISearchable searchProblem, AState start) {
         searchProblem.resetPossibleStates();
         for (int i = 0; i < searchProblem.getAllPossibleStates().size(); i++) {
             AState u = searchProblem.getAllPossibleStates().get(i);
             u.setColor(Color.white);
             u.setParent(null);
         }
-        time = 0;
-        for (int i = 0; i < searchProblem.getAllPossibleStates().size(); i++) {
-            AState u = searchProblem.getAllPossibleStates().get(i);
-            if(u.getColor() == Color.white)
-                DFSVisit(searchProblem, u);
-            if (u.node.getRowIndex() == searchProblem.getGoal().node.getRowIndex() && u.node.getColumnIndex() == searchProblem.getGoal().node.getColumnIndex())
-                searchProblem.setGoal(u);
-        }
+        Stack<AState> stack = new Stack<AState>();
+        stack.push(start);
+        while (!stack.isEmpty()) {
+            time++;
+            AState current = stack.pop();
+            if (current.getColor() == Color.white) {
+                current.setColor(Color.gray);
+                current.setDist(time);
+                searchProblem.setPossibleStates(current.getNeighbors());
+                List<AState> adjVertices = searchProblem.getAllPossibleStates();
+                for (AState v : adjVertices) {
+                    if (v.getColor() == Color.white)
+                    {
+                        stack.push(v);
+                        v.setParent(current);
+                    }
 
-
-    }
-
-    /**
-     * Helper function of DFS
-     * @param v to set dist, color and parent
-     */
-    private void DFSVisit(ISearchable searchProblem, AState v)
-    {
-        time += 1;
-        v.setDist(time);
-        v.setColor(Color.gray);
-        searchProblem.setPossibleStates(v.getNeighbors());
-        for (int i = 0; i < searchProblem.getAllPossibleStates().size(); i++) {
-            AState u = searchProblem.getAllPossibleStates().get(i);
-            if(u.getColor() == Color.white)
-            {
-                u.setParent(v);
-                DFSVisit(searchProblem, u);
+                }
             }
+            current.setColor(Color.black);
         }
-        v.setColor(Color.black);
-        time += 1;
-        //f[v] = time
     }
+
     @Override
     public String getName() {
         return "Depth First Search";
@@ -69,11 +66,11 @@ public class DepthFirstSearch extends ASearchingAlgorithm{
 
     @Override
     public Solution solve(ISearchable domain) {
-        DFS(domain);
+        DFS(domain, domain.getStart());
         Solution solution = new Solution();
         solution.addToPath(domain.getGoal()); // start from the end to promise a legal path
         AState currentState = domain.getGoal();
-        while(currentState.getParent() != null){ // run until start (parent is null)
+        while (currentState.getParent() != null) { // run until start (parent is null)
             solution.addToPath(currentState.getParent());
             currentState.setCost();
             numOfNode++;
@@ -81,4 +78,51 @@ public class DepthFirstSearch extends ASearchingAlgorithm{
         }
         return solution;
     }
+
+
 }
+
+
+
+// OLD DFS
+//    private void DFS(ISearchable searchProblem)
+//    {
+//        searchProblem.resetPossibleStates();
+//        for (int i = 0; i < searchProblem.getAllPossibleStates().size(); i++) {
+//            AState u = searchProblem.getAllPossibleStates().get(i);
+//            u.setColor(Color.white);
+//            u.setParent(null);
+//        }
+//        time = 0;
+//        for (int i = 0; i < searchProblem.getAllPossibleStates().size(); i++) {
+//            AState u = searchProblem.getAllPossibleStates().get(i);
+//            if(u.getColor() == Color.white)
+//                DFSVisit(searchProblem, u);
+//            if (u.node.getRowIndex() == searchProblem.getGoal().node.getRowIndex() && u.node.getColumnIndex() == searchProblem.getGoal().node.getColumnIndex())
+//                searchProblem.setGoal(u);
+//        }
+//
+//
+//    }
+//
+//    /**
+//     * Helper function of DFS
+//     *
+//     * @param v to set dist, color and parent
+//     */
+//    private void DFSVisit(ISearchable searchProblem, AState v) {
+//        time += 1;
+//        v.setDist(time);
+//        v.setColor(Color.gray);
+//        searchProblem.setPossibleStates(v.getNeighbors());
+//        for (int i = 0; i < searchProblem.getAllPossibleStates().size(); i++) {
+//            AState u = searchProblem.getAllPossibleStates().get(i);
+//            if (u.getColor() == Color.white) {
+//                u.setParent(v);
+//                DFSVisit(searchProblem, u);
+//            }
+//        }
+//        v.setColor(Color.black);
+//        time += 1;
+//        //f[v] = time
+//    }
