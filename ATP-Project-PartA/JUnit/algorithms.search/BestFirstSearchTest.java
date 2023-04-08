07d39994ae19941b1f7037b7de93d3639aed0173
RotemAmir22 <100492679+RotemAmir22@users.noreply.github.com>
2023-04-08 12:48:44 +0300

    JUnit

---
+++ ATP-Project-PartA/JUnit/algorithms.search/BestFirstSearchTest.java
@@ -0,0 +1,14 @@
+package algorithms.search;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BestFirstSearchTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void solve() {
+    }
+}
